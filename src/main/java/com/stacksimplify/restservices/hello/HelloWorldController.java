3da1d8df66f1e3c69224aa4a28b084437abfb924
vijithcprm <vijithcprm@gmail.com>
2021-02-02 19:00:45 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/hello/HelloWorldController.java
@@ -0,0 +1,22 @@
+package com.stacksimplify.restservices.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+	//@RequestMapping(method = RequestMethod.GET, path = "/helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld() {
+		return "Hello World1";
+	}
+	
+	@GetMapping("/helloWorldBean")
+	public UserDetails helloWorldBean() {
+		return new UserDetails("vijith","kumar","tamilnadu");
+	}
+
+}

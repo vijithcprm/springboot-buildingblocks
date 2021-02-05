174f3ec867ba3bc27700f1efad993370bdcaebe4
vijithcprm <vijithcprm@gmail.com>
2021-02-05 16:46:24 +0530

    ModelMapper first commit

---
+++ src/main/java/com/stacksimplify/restservices/dtos/UserMmDto.java
@@ -0,0 +1,27 @@
+package com.stacksimplify.restservices.dtos;
+
+public class UserMmDto {
+	
+	private Long userid;
+	
+	private String username;
+
+	public Long getUserid() {
+		return userid;
+	}
+
+	public void setUserid(Long userid) {
+		this.userid = userid;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	
+	
+
+}
